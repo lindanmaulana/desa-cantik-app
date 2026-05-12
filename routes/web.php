e35c6cd8e1ffb3c5531953996c28c93c760b7239
LindanMaulana <linmidofficial@gmail.com>
2026-05-12 14:44:12 +0700

    feat(dashboard/statistics): added routing and demographics

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Statistics\DemographController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('statistics')->group(function () {
+    Route::get('/demograph', [DemographController::class, 'index']);
 });
